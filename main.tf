1b6cf906078a4af948a2bf76f30118db74da0f8f
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-06-18 06:48:58 +0500

    Update backend to correct point

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "~> 4.15.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.5.1"
     }
   }
-  required_version = "~> 1.0"
+  required_version = "~> 1.4.6"
 
-  backend "remote" {
-    organization = "REPLACE_ME"
+  cloud {
+    organization = "github-actions-terraform"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "github-actions-demo"
     }
   }
 }
@@ -27,30 +27,30 @@ provider "aws" {
 
 
 
-resource "random_pet" "sg" {}
+# resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+# resource "aws_instance" "web" {
+#   ami                    = "ami-09e67e426f25ce0d7"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
+# }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "web-sg" {
+#   name = "${random_pet.sg.id}-sg"
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
+# output "web-address" {
+#   value = "${aws_instance.web.public_dns}:8080"
+# }
